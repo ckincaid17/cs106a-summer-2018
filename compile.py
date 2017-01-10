c28206cbbbeeb3e8935432961e289ae7ddb8b625
chrispiech <cpiech@stanford.edu>
2017-01-09 17:38:00 -0800

    a

--- compile.py
+++ compile.py
@@ -9,7 +9,7 @@ IGNORE_DIRS = [
     'parts'
 ]
 TEMPLATE_DIR = 'templates'
-ROOT = 'http://web.stanford.edu/class/archive/cs/cs106a/cs106a.1174/'
+ROOT = 'http://web.stanford.edu/class/cs/cs106a/'
 
 # Use the -t flag if you want to compile for local tests
 DEPLOY = not '-t' in sys.argv
