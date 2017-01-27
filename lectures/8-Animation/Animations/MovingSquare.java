190c8d7002c59337174dcb2c6cd867e952fe743c
chrispiech <cpiech@stanford.edu>
2017-01-27 09:42:28 -0800

    animation

---
+++ lectures/8-Animation/Animations/MovingSquare.java
@@ -0,0 +1,22 @@
+/*
+ * File: DrawFlag.java
+ * -----------------------------
+ * Draws various flags from around the world (that have
+ * only horizontal bands)
+ */
+
+import java.awt.Color;
+
+import acm.graphics.*;
+import acm.program.*;
+
+public class MovingSquare extends GraphicsProgram {
+
+	private static final int DELAY = 3;
+	private static final int SQUARE_SIZE = 100;
+	
+	public void run() {	
+		// your code here
+	}
+
+}
