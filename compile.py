79ccbcf529f6ccbff37faf44b66b3c3357de0361
Nick Troccoli <nicktroccoli28@gmail.com>
2017-06-28 00:30:16 -0700

    Hide assignments handouts from handouts dropdown

--- compile.py
+++ compile.py
@@ -94,7 +94,8 @@ def searchHandoutsDirectory():
     handoutsDirPath = OUTPUT_DIR + '/' + HANDOUTS_DIR + '/'
     for fileName in os.listdir(handoutsDirPath):
         filePath = os.path.join(HANDOUTS_DIR, fileName)
-        if os.path.isdir(filePath) or fileName.startswith("."): continue
+        if fileName == "assignments" or fileName.startswith("."):
+            continue
 
         # The name is found by separating each dashed word in the filename
         # except for the first, which is the handout number.  E.g.
