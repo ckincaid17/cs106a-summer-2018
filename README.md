8a6b2c0c37c4d83091ce62c4cec855bf37b9cce2
Nick Troccoli <nicktroccoli28@gmail.com>
2018-01-09 23:22:09 -0800

    Compiled typo fixes

---
+++ README.md
@@ -0,0 +1,152 @@
+# CS106A Summer 2017 Website
+
+This is a pre-compiled, static website for the Summer 2017 offering of
+Stanford's CS106A introductory programming course.  This repository is structured as
+follows:
+
+```
+bottle/ - the source files for Bottle, the library used to compile templates
+templates/ - the uncompiled Bottle templates (.html represent actual pages,
+.ptl represent 'partials' that are used within other files)
+WWW/ - the output folder containing compiled pages and other static resources
+compile.py - the template compilation script
+```
+
+(Note: for hosting on Stanford servers, it's easiest to symlink from the course's WWW/ folder to the above WWW/ folder)
+
+## Editing
+Within `templates`, there are subfolders for exam pages, assignment pages, and
+subparts of various pages.  Here are the relevant files for various tasks you
+may want to do:
+
+- **Initial setup and customization:** see `courseInfo.py` for various constants you must specify containing information about your course offering.  These constants define content displayed on various pages; info about these constants is documented within the file.
+
+- **Posting announcements:** add a `<div class="well">` element to
+`templates/announcements.html` containing your announcement.  See existing
+announcements, as well as `templates/announcements-old.html`, for example
+formatting.
+
+- **Adding handouts (excluding section materials):** add the handout to `WWW/handouts/`, with the
+naming style `NUMBER-DASHED_NAME.extension`.  E.g. `1-General-Information.pdf`.
+Any handouts in this folder will be automatically listed in the "Handouts"
+dropdown with the format `NUMBER - SEPARATED_NAME`.
+E.g. `1 - General Information`.
+
+- **Adding section materials:** make a new folder of that section number in `WWW/section/` and add
+materials according to the following naming conventions:
+	- `Section[NUM].pdf` - handout
+	- `Section[NUM].zip` - solution code
+	- `Section[NUM]-Solutions.pdf` - solution handout
+
+For instance, for section 6 you would add `Section6.pdf`, `Section6.zip` and `Section6-Solutions.pdf`.
+
+You must also add an `info.json` file to the directory that looks as follows:
+```
+{
+	"solutionsDate": "2017063017"
+}
+```
+This timestamp should be when the solution materials will be made available (in the format YYYYMMDDHH).
+The solution handout and code will be visible in the Section dropdown from that
+date onwards.  The section handout will be visible as soon as it is added to the
+repository.
+
+- **Add a new assignment:** add the assignment page template to
+`templates/assignments/` and update
+`templates/navbar/assignmentsDropdownList.html` to list the new
+assignment.
+
+- **Change the lecture schedule:** the lecture calendar and the lectures
+dropdown are both dynamically generated from the same JSON file,
+`schedule.json`.  This JSON file has the following format:
+
+```
+{
+	"days": ...,
+	"weeks": ...
+}
+```
+
+`days` maps to an array of strings naming each day on which there is lecture.
+This is displayed as the header row in the schedule table.  E.g.
+
+```
+{
+	"days": ["Monday", "Tuesday", "Wednesday", "Thursday"],
+	"weeks": ...
+}
+```
+
+`weeks` is a list containing data for each week.  Each week is itself a list of
+objects representing each lecture that week.  Note that each week's length must
+match the length of the `days` array.  (The reason for this is the added flexibility
+of having assignments due on non-lecture days.)  These lecture objects have the following
+keys (whose values are all strings):
+```
+date (REQUIRED): the date string (e.g. "June 26") to display
+title (OPTIONAL): the name of the lecture (or event)
+type (OPTIONAL): "HOLIDAY" if a holiday, or "OFF" if no lecture
+filename (OPTIONAL): the filename prefix for all material filepaths (if not provided, neither code nor slides are linked to)
+code (OPTIONAL): if false, does not show the "Code" link for this day
+slides (OPTIONAL): if false, does not show the "Slides" links for this day
+notes (OPTIONAL): a list of strings to display in gray on this calendar day,
+				 each on its own line.
+practice (OPTIONAL): a list of length-2 lists ([PROBLEM NAME, URL]) representing that day's practice problems. 
+```
+
+Of note, `filename` is used as follows:
+- the lecture PPT is assumed to be at ```{{pathToRoot}}lectures/{{filename}}/{{filename}}.pptx```
+- the lecture PDF is assumed to be at ```{{pathToRoot}}lectures/{{filename}}/{{filename}}.pdf```
+- the lecture code ZIP is assumed to be at ```{{pathToRoot}}lectures/{{filename}}/{{filename}}.zip```
+
+Holidays are grayed out in the calendar.  All
+information is dynamically displayed from this JSON file for each lecture,
+including date, title, links to material, any HWs due, and any reading due;
+lectures are also numbered automatically.
+
+All lectures in this file are also displayed in the "Lectures" dropdown in the
+navbar, up to the first lecture without a filename.  Dividers are added between
+weeks.
+
+
+## Compiling
+To compile all references and links to test locally, from the root directory run
+
+```
+python compile.py -t --output_dir myDirectory
+```
+
+This compiles all templates in the `templates/` folder ending with .html (.ptl
+files are partial templates) and, preserving its directory structure and
+filenames, creates all HTML files in the given output directory.  If no output
+directory is specified, the files are outputted to the ```WWW/``` directory.
+
+To compile all references and links to host, from the root directory run
+
+```
+python compile.py --output_dir myDirectory
+```
+
+This does the same thing as the previous command, but compiles all website URLs
+based on the given ```ROOT``` constant in `compile.py` instead of local paths.
+
+You may also change any of the default compilation settings in `compile.py`,
+including:
+
+```
+ROOT - the root URL for the hosted version of this website
+OUTPUT_DIR - the default output directory for compiled files
+TEMPLATE_DIR - the directory of templates to compile
+HANDOUTS_DIR - the directory within OUTPUT_DIR containing handout PDFs
+SECTION_DIR - the directory within OUTPUT_DIR containing each week's section materials
+```
+
+
+## Testing Locally
+To host the compiled site locally, from the `WWW/` directory run
+
+`python -m SimpleHTTPServer`
+
+This will host the site on `localhost:8000`.  Make sure to compile using the
+`-t` flag beforehand; otherwise the paths will not function properly!
+
---
+++ courseInfo.py
@@ -0,0 +1,171 @@
+# General course information
+COURSE_NUMBER = "CS 106A"
+COURSE_NAME = "Programming Methodology"
+COURSE_TITLE = COURSE_NUMBER + ": " + COURSE_NAME
+CS198_NUMBER = 1 # B = 2, X = 3
+
+# General quarter information
+QUARTER_NUMBER = 1178
+QUARTER_NAME = "Summer"
+QUARTER_YEAR = "2017"
+QUARTER_FULL_NAME = QUARTER_NAME + " " + QUARTER_YEAR
+QUARTER_OVER = True  # Set to True to add banner at the top redirecting to newer version of webpage
+
+# Lecture information (displayed at top of home page)
+LECTURE_DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday"]
+LECTURE_TIME = "11:30AM-12:20PM PST"
+LECTURE_LOCATION = "NVIDIA Auditorium"
+LECTURE_LOCATION_LINK = "https://www.google.com/maps/place/NVIDIA+Auditorium/@37.4277284,-122.1763905,17z/data=!3m1!4b1!4m5!3m4!1s0x808fbb2ac97723cb:0xa6e8fc1a7a5f2c29!8m2!3d37.4277284!4d-122.1741965"
+LECTURE_FEEDBACK_LINK = "https://goo.gl/forms/viRkXgYxDRq0Uorm2" # Length > 0
+
+# Course staff (displayed in left sidebar and on staff page)
+STAFF_INFO = [
+	{
+		"POSITION": "Instructor",
+		"NAME": "Nick Troccoli",
+		"IMAGE": "troccoli.jpg",
+		"EMAIL": "troccoli@stanford.edu",
+		"OFFICE_HOURS_LOCATION": "Gates B02",
+		"OFFICE_HOURS_DATETIME": "Wed. + Thurs. 2-4PM",
+		"DESCRIPTION": "The instructor teaches lectures and manages the overall course. Contact the instructor if you have a question that cannot be answered by your section leader; the SLs are your primary email contact for all questions about homework or other course issues.  Please also cc your section leader and/or Head TA.  In addition to at his office hours, please feel free to ask Nick questions before/after any lecture."
+	},
+	{
+		"POSITION": "Head TA",
+		"NAME": "Rishi Bedi",
+		"IMAGE": "rbedi.jpg",
+		"EMAIL": "rbedi@stanford.edu",
+		"OFFICE_HOURS_LOCATION": "Gates B02",
+		"OFFICE_HOURS_DATETIME": "Tues. + Thurs. 9-11AM",
+		"DESCRIPTION": "The Head TA manages the section leaders and also oversees important course activities such as grading and section problems. Contact the Head TA if you need a regrade on an assignment, or have a question that cannot be answered by your section leader; the SLs are your primary email contact for all questions about homework or other course issues.  Please also cc your section leader and/or Instructor."
+	}
+]
+
+# Note: section leader names MUST be formatted "NAME (SUNET)" as the image for a given Section Leader is assumed to be SUNET.jpg.
+SECTION_LEADERS = [
+	"Aleksander Dash (adash)",
+	"Canyon Robins (crobins)",
+	"Conner Smith (csmith95)",
+	"Emily Ling (eling8)",
+	"Farah Uraizee (farah)",
+	"Garrick Fernandez (gfaerr)",
+	"Gus Torres da Silva (gdasilva)",
+	"Guy Blanc (gblanc)",
+	"Jared Bitz (jbitz)",
+	"Jestin Ma (jestinm)",
+	"Kate Rydberg (rydbergk)",
+	"Katherine Erdman (kerdman)",
+	"Keanu Spies (keanus)",
+	"Michael Hazard (cmhazard)",
+	"Mirae Parker (miraep8)",
+	"Nick Negrete (nnegrete)",
+	"Nolan Handali (nolanh)",
+	"Pras Ramakrishnan (pras1712)",
+	"Regina Nguyen (renguyen)",
+	"Tobin Bell (trfbell)",
+	"Tyler Yep (tyep)",
+	"Wil Kautz (wkautz)"
+]
+
+# SCPD (listed in left sidebar)
+SCPD_OFFICE_HOURS = [
+	"Sun 5:30-7PM PST",
+	"Tues 9-11AM PST",
+	"Tues 8-9PM PST",
+	"Wed 8:30-10PM PST",
+	"Thurs 4-5PM PST",
+	"Thurs 8:30-9:30PM PST"
+]
+SCPD_OFFICE_HOURS_LINK = "http://www.queuestatus.com/queues/122"
+SCPD_OFFICE_HOURS_DESCRIPTION = "SCPD students can sign up for help at the times above (free QueueStatus account required)."
+
+# Exams (displayed in left sidebar, but locations and review session are only displayed on exam subpages)
+MIDTERM = {
+	"DATE": "Monday, July 24",
+	"TIME": "7-9PM PST",
+	"LOCATIONS": [ 	# A list of objects containing TITLE, LOCATION, MAP_LINK
+		{
+			"TITLE": "Exam",
+			"LOCATION": "Hewlett 200",
+			"MAP_LINK": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3168.345197243158!2d-122.1750263848679!3d37.42894923975373!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fbb2ae177cc91%3A0x5351ffed56456da8!2sWilliam+R.+Hewlett+Teaching+Center%2C+370+Serra+Mall%2C+Stanford%2C+CA+94305!5e0!3m2!1sen!2sus!4v1500400060838"
+		}
+	],
+	"REVIEW_SESSION": {
+		"DATE": "Friday, July 21",
+		"TIME": "3:30-4:50PM PST",
+		"LOCATION": "Gates B03 (basement)",
+		"MAP_LINK": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3168.2942783500653!2d-122.17568493444799!3d37.4301523298236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fbb2b3f50f727%3A0xfd9cc3200ee97fda!2sGates+Computer+Science%2C+353+Serra+Mall%2C+Stanford%2C+CA+94305!5e0!3m2!1sen!2sus!4v1500425297459"
+	}
+}
+
+FINAL_EXAM = {
+	"DATE": "Friday, August 18",
+	"TIME": "12:15-3:15PM PST",
+	"LOCATIONS": [	# A list of objects containing TITLE, LOCATION, MAP_LINK
+		{
+			"TITLE": "Last Names A-O",
+			"LOCATION": "NVIDIA Auditorium",
+			"MAP_LINK": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3168.396866793715!2d-122.17639054916224!3d37.42772837972516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fbb2ac97723cb%3A0xa6e8fc1a7a5f2c29!2sNVIDIA+Auditorium!5e0!3m2!1sen!2sus!4v1502919715852"
+		},
+		{
+			"TITLE": "Last Names P-Z",
+			"LOCATION": "Gates B01 (Basement)",
+			"MAP_LINK": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3168.2942783500653!2d-122.17568493444799!3d37.4301523298236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fbb2b3f50f727%3A0xfd9cc3200ee97fda!2sGates+Computer+Science%2C+353+Serra+Mall%2C+Stanford%2C+CA+94305!5e0!3m2!1sen!2sus!4v1500425297459"
+		}
+	],
+	"REVIEW_SESSION": {
+		"DATE": "Mon/Tue, August 14-15",
+		"TIME": "11:30AM-12:20PM PST",
+		"LOCATION": "NVIDIA Auditorium",
+		"MAP_LINK": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3168.396866793715!2d-122.17639054916224!3d37.42772837972516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fbb2ac97723cb%3A0xa6e8fc1a7a5f2c29!2sNVIDIA+Auditorium!5e0!3m2!1sen!2sus!4v1502919715852"
+	}
+}
+
+# Assignments (displayed in Assignments dropdown at the top)
+STYLE_GUIDE = True
+PAIR_PROGRAMMING = True
+ASSIGNMENTS = [
+	{
+		"NAME": "Karel the Robot",
+		"LINK": "karel.html"
+	},
+	{
+		"NAME": "Intro to Java",
+		"LINK": "introToJava.html"
+	},
+	{
+		"NAME": "Hangman",
+		"LINK": "hangman.html"
+	},
+	{
+		"NAME": "Artistry and Breakout",
+		"LINK": "breakout.html"
+	},
+	{
+		"NAME": "ImageShop",
+		"LINK": "imageshop.html"
+	},
+	{
+		"NAME": "NameSurfer",
+		"LINK": "namesurfer.html"
+	}
+]
+ASSIGNMENT_DUE_TIME = "11AM PST"
+
+# Software
+SOFTWARE = "Eclipse"
+SOFTWARE_ICON = "eclipse-icon.png"
+SOFTWARE_LINK = "eclipse"
+
+# Stanford Library
+LIBRARY_DOCS_LINK = "http://stanford.edu/~stepp/acmdoc/"
+
+BLANK_PROJECTS = [
+	{
+		"NAME": "Blank Karel Project",
+		"LINK": "/resources/BlankKarelProject.zip"
+	},
+	{
+		"NAME": "Blank Java Project",
+		"LINK": "/resources/BlankJavaProject.zip"
+	}
+]
