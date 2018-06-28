c72587d54377abea572b891d5fdeba4f443dc708
Annie Hu <anniehu4@gmail.com>
2018-06-28 10:49:00 -0700

    updating SCPD site + adding note for missing lecture assignments

--- courseInfo.py
+++ courseInfo.py
@@ -80,7 +80,7 @@ SCPD_OFFICE_HOURS = [
 	# "Thurs 8:30-9:30PM PST"
 ]
 SCPD_OFFICE_HOURS_LINK = "http://www.queuestatus.com/queues/225"
-SCPD_OFFICE_HOURS_DESCRIPTION = "for help during the times above (free QueueStatus account required)."
+SCPD_OFFICE_HOURS_DESCRIPTION = "for help during the times below (free QueueStatus account required)."
 
 # Exams (displayed in left sidebar, but locations and review session are only displayed on exam subpages)
 MIDTERM = {
