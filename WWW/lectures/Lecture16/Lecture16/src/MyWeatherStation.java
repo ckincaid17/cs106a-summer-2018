dfca53ae8765acc87c19ffbccd2ce3f1412cce3e
Colin Kincaid <colin.kincaid.school@gmail.com>
2018-07-24 11:25:37 -0700

    Lecture 16

---
+++ WWW/lectures/Lecture16/Lecture16/src/MyWeatherStation.java
@@ -0,0 +1,36 @@
+/* File: MyWeatherStation
+ * --------------------
+ * This program shows how we can use arrays to do
+ * more complex data manipulation.  It prompts the user
+ * for a variable number of daily temperatures, and then prints
+ * out all the entered temperatures along with statistics such as
+ * the average, number of days above average,
+ * and the hottest and coldest days.
+ * --------------------
+ */
+
+import acm.program.*;
+import java.util.*;
+
+public class MyWeatherStation extends ConsoleProgram {
+	public void run() {
+		// 1) Read in temperatures from the user
+		
+		// 2) Calculate statistics with average
+
+		// 3) Print out two coldest and two hottest days		
+	}
+	
+	
+	/* Reads in daily temperatures from the user.
+	 * Returns an array of all entered temperatures.
+	 */
+	
+	
+	/* Returns the average value in the temperatures array.
+	 */
+	
+	
+	/* Returns the number of temperatures above the given average.
+	 */
+}
